59e48f8a4453abbf32e2d90b3f10adb7f9da4619
nathan-casabieille <nathan.casabieille1@gmail.com>
2026-05-10 18:55:24 +0200

    Match 10 more one-off stubs (misc_matchesF) size 32-45
    712 -> 722 matched.

---
+++ src/engine/misc_matchesF.c
@@ -0,0 +1,273 @@
+/**
+ * Fifteenth batch of one-off matches.
+ */
+#include "engine/scenegraph.h"
+#include "game/tick.h"
+
+extern unsigned int g_baseSel_00542060;
+extern unsigned int g_scaledInit_00542044;
+
+/* @addr 0x00422880 (44b)
+ *   call    F
+ *   mov     eax, [g_framePauseFlag]
+ *   test    eax, eax
+ *   jne     .ret
+ *   mov     eax, [g_eventQueueCurrent]
+ *   mov     ecx, [g_xformEntityIdx]
+ *   inc     eax
+ *   mov     [g_eventQueueCurrent], eax
+ *   mov     [eax*4 + 0], ecx
+ *   jmp     T
+ */
+extern unsigned int g_xformEntityIdx;
+extern void func_0048fbf0(void);
+extern void func_00478328(void);
+__declspec(naked) void CallPauseEvtPushJmp_00422880(void) {
+    __asm {
+        call    func_0048fbf0
+        mov     eax, dword ptr [g_framePauseFlag]
+        test    eax, eax
+        _emit   75h
+        _emit   1dh
+        mov     eax, dword ptr [g_eventQueueCurrent]
+        mov     ecx, dword ptr [g_xformEntityIdx]
+        inc     eax
+        mov     dword ptr [g_eventQueueCurrent], eax
+        mov     dword ptr [ecx*4 + 0], eax
+        jmp     func_00478328
+        ret
+    }
+}
+
+/* @addr 0x004297d0 (45b)
+ *   mov     eax, [g_fightGroupHead]
+ *   mov     ecx, [g_scaledInit_00542044]
+ *   mov     eax, [eax*4 + 0x28]
+ *   inc     eax
+ *   mov     [g_eventQueueCurrent], eax
+ *   mov     edx, [ecx*4 + 4]
+ *   cmp     eax, edx
+ *   jge     +5
+ *   jmp     T1
+ *   jmp     T2
+ */
+extern void func_004296d8(void);
+extern void func_004296e8(void);
+__declspec(naked) void ScaledIncCmpJmp_004297d0(void) {
+    __asm {
+        mov     eax, dword ptr [g_fightGroupHead]
+        mov     ecx, dword ptr [g_scaledInit_00542044]
+        mov     eax, dword ptr [eax*4 + 0x28]
+        inc     eax
+        mov     dword ptr [g_eventQueueCurrent], eax
+        mov     edx, dword ptr [ecx*4 + 4]
+        cmp     eax, edx
+        _emit   7dh
+        _emit   05h
+        jmp     func_004296d8
+        jmp     func_004296e8
+    }
+}
+
+/* @addr 0x0042c9c0 (35b)
+ *   push    0x004e3540
+ *   call    F
+ *   mov     eax, [g_framePauseFlag]
+ *   add     esp, 4
+ *   test    eax, eax
+ *   jne     .ret
+ *   mov     eax, [g_xformDirtyFlags]
+ *   and     al, 0xfe
+ *   mov     [g_xformDirtyFlags], eax
+ *   ret
+ */
+extern int func_004594f0(void *);
+extern void *g_data_004e3540;
+__declspec(naked) void PushCallPauseDirtyClear_0042c9c0(void) {
+    __asm {
+        push    OFFSET g_data_004e3540
+        call    func_004594f0
+        mov     eax, dword ptr [g_framePauseFlag]
+        add     esp, 4
+        test    eax, eax
+        _emit   75h
+        _emit   0ch
+        mov     eax, dword ptr [g_xformDirtyFlags]
+        and     al, 0xfe
+        mov     dword ptr [g_xformDirtyFlags], eax
+        ret
+    }
+}
+
+/* @addr 0x0042ee10 (36b)
+ *   mov     eax, [0x0052aac4]
+ *   mov     [g_walkCallback], eax
+ *   cmp     eax, 7
+ *   mov     eax, [g_xformDirtyFlags]
+ *   je      +8
+ *   and     al, 0xfe
+ *   mov     [g_xformDirtyFlags], eax
+ *   ret
+ *   or      al, 1
+ *   mov     [g_xformDirtyFlags], eax
+ *   ret
+ */
+extern unsigned int g_state_0052aac4;
+__declspec(naked) void Cmp7DirtyToggle_0042ee10(void) {
+    __asm {
+        mov     eax, dword ptr [g_state_0052aac4]
+        mov     dword ptr [g_walkCallback], eax
+        cmp     eax, 7
+        mov     eax, dword ptr [g_xformDirtyFlags]
+        _emit   74h
+        _emit   08h
+        and     al, 0xfe
+        mov     dword ptr [g_xformDirtyFlags], eax
+        ret
+        or      al, 1
+        mov     dword ptr [g_xformDirtyFlags], eax
+        ret
+    }
+}
+
+/* @addr 0x004338e0 (34b)
+ *   mov     eax, [g_state_0053a3c0]
+ *   cmp     eax, 3
+ *   mov     [g_walkCallback], eax
+ *   jle     +5
+ *   jmp     T1
+ *   push    0x004e44c0
+ *   call    F
+ *   add     esp, 4
+ *   ret
+ */
+extern unsigned int g_state_0053a3c0;
+extern void func_00433901(void);
+extern int func_004392a0(void *);
+extern void *g_data_004e44c0;
+__declspec(naked) void Cmp3JmpOrPushCall_004338e0(void) {
+    __asm {
+        mov     eax, dword ptr [g_state_0053a3c0]
+        cmp     eax, 3
+        mov     dword ptr [g_walkCallback], eax
+        _emit   7eh
+        _emit   05h
+        jmp     func_00433901
+        push    OFFSET g_data_004e44c0
+        call    func_004392a0
+        add     esp, 4
+        ret
+    }
+}
+
+/* @addr 0x00433e50 (32b)
+ *   mov     eax, [g_eventQueueCurrent]
+ *   mov     ecx, 0x004e4cf8
+ *   shr     eax, 8
+ *   shr     ecx, 2
+ *   mov     [g_eventQueueCurrent], eax
+ *   mov     [g_eventQueueChild], ecx
+ *   jmp     T
+ */
+extern void func_00433e80(void);
+__declspec(naked) void ShrShrStoreJmp_00433e50(void) {
+    __asm {
+        mov     eax, dword ptr [g_eventQueueCurrent]
+        mov     ecx, 0x004e4cf8
+        shr     eax, 8
+        shr     ecx, 2
+        mov     dword ptr [g_eventQueueCurrent], eax
+        mov     dword ptr [g_eventQueueChild], ecx
+        jmp     func_00433e80
+    }
+}
+
+/* @addr 0x00433e70 (32b)
+ *   mov     eax, [g_eventQueueChild]
+ *   mov     ecx, [g_eventQueueCurrent]
+ *   add     eax, ecx
+ *   mov     [g_eventQueueChild], eax
+ *   mov     eax, [eax*4 + 0]
+ *   mov     [g_eventQueueChild], eax
+ *   jmp     eax
+ */
+__declspec(naked) void AddDerefJmp_00433e70(void) {
+    __asm {
+        mov     eax, dword ptr [g_eventQueueChild]
+        mov     ecx, dword ptr [g_eventQueueCurrent]
+        add     eax, ecx
+        mov     dword ptr [g_eventQueueChild], eax
+        mov     eax, dword ptr [eax*4 + 0]
+        mov     dword ptr [g_eventQueueChild], eax
+        jmp     eax
+    }
+}
+
+/* @addr 0x00438e70 (37b)
+ *   mov     eax, [g_matrixStackTop]
+ *   mov     [g_walkCallback], 3
+ *   inc     eax
+ *   mov     [g_matrixStackTop], eax
+ *   mov     [eax*4 + 0], OFFSET func_004391b0
+ *   jmp     T
+ */
+extern void func_0043928c(void);
+extern void func_004391b0(void);
+__declspec(naked) void MStackPushPtr1Jmp_00438e70(void) {
+    __asm {
+        mov     eax, dword ptr [g_matrixStackTop]
+        mov     dword ptr [g_walkCallback], 3
+        inc     eax
+        mov     dword ptr [g_matrixStackTop], eax
+        mov     dword ptr [eax*4 + 0], OFFSET func_004391b0
+        jmp     func_0043928c
+    }
+}
+
+/* @addr 0x00438ef0 (37b)
+ *   mov     eax, [g_matrixStackTop]
+ *   mov     [g_walkCallback], 1
+ *   inc     eax
+ *   mov     [g_matrixStackTop], eax
+ *   mov     [eax*4 + 0], OFFSET func_004391b0
+ *   jmp     T
+ */
+extern void func_0043930c(void);
+__declspec(naked) void MStackPushPtr1Jmp_00438ef0(void) {
+    __asm {
+        mov     eax, dword ptr [g_matrixStackTop]
+        mov     dword ptr [g_walkCallback], 1
+        inc     eax
+        mov     dword ptr [g_matrixStackTop], eax
+        mov     dword ptr [eax*4 + 0], OFFSET func_004391b0
+        jmp     func_0043930c
+    }
+}
+
+/* @addr 0x00438f20 (38b)
+ *   push    0x004e4a1c
+ *   call    F
+ *   mov     eax, [g_framePauseFlag]
+ *   add     esp, 4
+ *   test    eax, eax
+ *   jne     .ret
+ *   mov     [g_walkCallback], 1
+ *   jmp     T
+ */
+extern int func_00440950(void *p);
+extern void *g_data_004e4a1c;
+extern void func_0046f370(void);
+__declspec(naked) void PushCallPauseSet1Jmp_00438f20(void) {
+    __asm {
+        push    OFFSET g_data_004e4a1c
+        call    func_00440950
+        mov     eax, dword ptr [g_framePauseFlag]
+        add     esp, 4
+        test    eax, eax
+        _emit   75h
+        _emit   0fh
+        mov     dword ptr [g_walkCallback], 1
+        jmp     func_0046f370
+        ret
+    }
+}
