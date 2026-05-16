940f8ffbfdafb10a299e225c190842e48fb73987
nathan-casabieille <nathan.casabieille1@gmail.com>
2026-05-16 17:48:09 +0200

    triple_test_install_jmp x2: 3-store + call + pause + 3-way dispatch

--- src/engine/triple_test_install_jmp.c
+++ src/engine/triple_test_install_jmp.c
@@ -44,61 +44,49 @@ extern void AudioStateRemapB_004a04a0(void);
 extern void AudioInstallSelfChannel8_004a0520(void);
 
 /* @addr 0x004a0130 */
-__declspec(naked) void TripleTestInstallJmp_004a0130(void) {
-    __asm {
-        mov     eax, dword ptr [g_eventQueueEnd]
-        mov     dword ptr [g_eventQueueWorkType], eax
-        mov     dword ptr [g_acc_00542078], eax
-        call    AudioVoiceSequencerCluster_004a0d60
-        mov     eax, dword ptr [g_framePauseFlag]
-        test    eax, eax
-        _emit   75h
-        _emit   35h
-        mov     eax, dword ptr [g_dlMode]
-        test    eax, eax
-        mov     dword ptr [g_walkCallback], eax
-        _emit   75h
-        _emit   05h
-        jmp     AudioStateRemap_004a0190
-        mov     eax, dword ptr [g_state2_0053a1bc]
-        test    eax, eax
-        mov     dword ptr [g_walkCallback], eax
-        _emit   75h
-        _emit   05h
-        jmp     AudioStateRemap_004a0190
-        mov     eax, dword ptr [g_acc_00542078]
-        mov     dword ptr [g_finalDest_0053a178], eax
-        jmp     AudioInstallSelfShiftedChainInit_004a0210
-        ret
+void TripleTestInstallJmp_004a0130(void) {
+    unsigned int v;
+    v = g_eventQueueEnd;
+    g_eventQueueWorkType = v;
+    g_acc_00542078 = v;
+    AudioVoiceSequencerCluster_004a0d60();
+    if (g_framePauseFlag) return;
+    v = g_dlMode;
+    g_walkCallback = (void(*)(void))v;
+    if (!v) {
+        AudioStateRemap_004a0190();
+        return;
     }
+    v = g_state2_0053a1bc;
+    g_walkCallback = (void(*)(void))v;
+    if (!v) {
+        AudioStateRemap_004a0190();
+        return;
+    }
+    g_finalDest_0053a178 = g_acc_00542078;
+    AudioInstallSelfShiftedChainInit_004a0210();
 }
 
 /* @addr 0x004a0440 */
-__declspec(naked) void TripleTestInstallJmp_004a0440(void) {
-    __asm {
-        mov     eax, dword ptr [g_eventQueueEnd]
-        mov     dword ptr [g_eventQueueWorkType], eax
-        mov     dword ptr [g_acc_00542078], eax
-        call    AudioVoiceSequencerCluster_004a0d60
-        mov     eax, dword ptr [g_framePauseFlag]
-        test    eax, eax
-        _emit   75h
-        _emit   35h
-        mov     eax, dword ptr [g_dlMode]
-        test    eax, eax
-        mov     dword ptr [g_walkCallback], eax
-        _emit   75h
-        _emit   05h
-        jmp     AudioStateRemapB_004a04a0
-        mov     eax, dword ptr [g_state2_0053a354]
-        test    eax, eax
-        mov     dword ptr [g_walkCallback], eax
-        _emit   75h
-        _emit   05h
-        jmp     AudioStateRemapB_004a04a0
-        mov     eax, dword ptr [g_acc_00542078]
-        mov     dword ptr [g_finalDest_0053a250], eax
-        jmp     AudioInstallSelfChannel8_004a0520
-        ret
+void TripleTestInstallJmp_004a0440(void) {
+    unsigned int v;
+    v = g_eventQueueEnd;
+    g_eventQueueWorkType = v;
+    g_acc_00542078 = v;
+    AudioVoiceSequencerCluster_004a0d60();
+    if (g_framePauseFlag) return;
+    v = g_dlMode;
+    g_walkCallback = (void(*)(void))v;
+    if (!v) {
+        AudioStateRemapB_004a04a0();
+        return;
     }
+    v = g_state2_0053a354;
+    g_walkCallback = (void(*)(void))v;
+    if (!v) {
+        AudioStateRemapB_004a04a0();
+        return;
+    }
+    g_finalDest_0053a250 = g_acc_00542078;
+    AudioInstallSelfChannel8_004a0520();
 }
