8bab9feecf8362501befe3cf4ecaba082a8e9206
nathan-casabieille <nathan.casabieille1@gmail.com>
2026-05-17 02:11:05 +0200

    naked -> pure C: ScaledInitWithCounterAndType_00446940
    scaled-init counter + self-install + type-28 pattern.

--- src/engine/misc_matchesU.c
+++ src/engine/misc_matchesU.c
@@ -174,18 +174,13 @@ __declspec(naked) void ScaledChainTripleStore_004465c0(void) {
 }
 
 /* @addr 0x00446940 (53b): same as 0x0041f1b0 but with diff IMM and value */
-__declspec(naked) void ScaledInitWithCounterAndType_00446940(void) {
-    __asm {
-        mov     eax, dword ptr [g_baseSel_00542060]
-        mov     ecx, 1
-        shl     eax, 2
-        mov     dword ptr [eax + 0x84], 0
-        mov     dword ptr [eax + 8], 0x00446940
-        mov     dword ptr [eax + 0x84], ecx
-        mov     dword ptr [g_pendingNodeType], 0x28
-        mov     dword ptr [g_framePauseFlag], ecx
-        ret
-    }
+void ScaledInitWithCounterAndType_00446940(void) {
+    unsigned char *base = (unsigned char *)(g_baseSel_00542060 * 4);
+    *(unsigned int *)(base + 0x84) = 0;
+    *(unsigned int *)(base + 8) = (unsigned int)ScaledInitWithCounterAndType_00446940;
+    *(unsigned int *)(base + 0x84) = 1;
+    g_pendingNodeType = 0x28;
+    g_framePauseFlag = 1;
 }
 
 /* @addr 0x004535e0 (53b)
