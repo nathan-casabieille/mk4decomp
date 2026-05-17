342ad1d77661f82e4235da59fa674aaed8c2086f
nathan-casabieille <nathan.casabieille1@gmail.com>
2026-05-17 08:41:13 +0200

    naked -> pure C: ScaledChain3c74_0048f910 + ScaledChain3c7c_0048f930
    scaled-chain 3c then 74/7c; bump K reloc_sites by +2 for 2 .debug$F.

--- src/engine/misc_matchesK.c
+++ src/engine/misc_matchesK.c
@@ -16,27 +16,17 @@ extern unsigned int g_scaledInit_00542044;
  *   mov     [g_walkCallback], ecx
  *   ret
  */
-__declspec(naked) void ScaledChain3c74_0048f910(void) {
-    __asm {
-        mov     eax, dword ptr [g_baseSel_00542060]
-        mov     eax, dword ptr [eax*4 + 0x3c]
-        mov     dword ptr [g_scaledInit_00542044], eax
-        mov     ecx, dword ptr [eax*4 + 0x74]
-        mov     dword ptr [g_walkCallback], ecx
-        ret
-    }
+void ScaledChain3c74_0048f910(void) {
+    unsigned int v = *(unsigned int *)(g_baseSel_00542060 * 4 + 0x3c);
+    g_scaledInit_00542044 = v;
+    g_walkCallback = (void (*)(void))*(unsigned int *)(v * 4 + 0x74);
 }
 
 /* @addr 0x0048f930 (31b): same shape with offset 0x7c instead of 0x74 */
-__declspec(naked) void ScaledChain3c7c_0048f930(void) {
-    __asm {
-        mov     eax, dword ptr [g_baseSel_00542060]
-        mov     eax, dword ptr [eax*4 + 0x3c]
-        mov     dword ptr [g_scaledInit_00542044], eax
-        mov     ecx, dword ptr [eax*4 + 0x7c]
-        mov     dword ptr [g_walkCallback], ecx
-        ret
-    }
+void ScaledChain3c7c_0048f930(void) {
+    unsigned int v = *(unsigned int *)(g_baseSel_00542060 * 4 + 0x3c);
+    g_scaledInit_00542044 = v;
+    g_walkCallback = (void (*)(void))*(unsigned int *)(v * 4 + 0x7c);
 }
 
 /* MStackPushSetImmJmp family - 7 variants of:
