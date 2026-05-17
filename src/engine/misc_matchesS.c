85925312d9c8cee5dcf9fba2f193820ad44b1097
nathan-casabieille <nathan.casabieille1@gmail.com>
2026-05-17 21:01:45 +0200

    S: convert Copy3Fields38_0040a870 to pure C

--- src/engine/misc_matchesS.c
+++ src/engine/misc_matchesS.c
@@ -84,23 +84,19 @@ __declspec(naked) void Copy3Fields3c4044_00404e90(void) {
  *   mov     [ecx + 0x40], eax
  *   ret
  */
-__declspec(naked) void Copy3Fields38_0040a870(void) {
-    __asm {
-        mov     eax, dword ptr [g_eventQueueEnd]
-        mov     ecx, dword ptr [g_scaledInit_00542044]
-        shl     eax, 2
-        shl     ecx, 2
-        mov     edx, dword ptr [eax]
-        mov     dword ptr [g_walkCallback], edx
-        mov     dword ptr [ecx + 0x38], edx
-        mov     edx, dword ptr [eax + 4]
-        mov     dword ptr [g_walkCallback], edx
-        mov     dword ptr [ecx + 0x3c], edx
-        mov     eax, dword ptr [eax + 8]
-        mov     dword ptr [g_walkCallback], eax
-        mov     dword ptr [ecx + 0x40], eax
-        ret
-    }
+void Copy3Fields38_0040a870(void) {
+    unsigned char *src = (unsigned char *)(g_eventQueueEnd * 4);
+    unsigned char *dst = (unsigned char *)(g_scaledInit_00542044 * 4);
+    unsigned int v;
+    v = *(unsigned int *)(src);
+    g_walkCallback = (void (*)(void))v;
+    *(unsigned int *)(dst + 0x38) = v;
+    v = *(unsigned int *)(src + 4);
+    g_walkCallback = (void (*)(void))v;
+    *(unsigned int *)(dst + 0x3c) = v;
+    v = *(unsigned int *)(src + 8);
+    g_walkCallback = (void (*)(void))v;
+    *(unsigned int *)(dst + 0x40) = v;
 }
 
 /* @addr 0x0041aa40 (60b)
