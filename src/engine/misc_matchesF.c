142ad1f2fb9ef3fe6ade8d38eed9825164b3c047
nathan-casabieille <nathan.casabieille1@gmail.com>
2026-05-17 09:31:20 +0200

    naked -> pure C: PushCallPauseSet1Jmp_00438f20
    Push arg + call-pause + set walkCallback=1 + tail-jmp.

--- src/engine/misc_matchesF.c
+++ src/engine/misc_matchesF.c
@@ -236,17 +236,9 @@ void MStackPushPtr1Jmp_00438ef0(void) {
 extern int func_00440950(void *p);
 extern void *g_data_004e4a1c;
 extern void func_0046f370(void);
-__declspec(naked) void PushCallPauseSet1Jmp_00438f20(void) {
-    __asm {
-        push    OFFSET g_data_004e4a1c
-        call    func_00440950
-        mov     eax, dword ptr [g_framePauseFlag]
-        add     esp, 4
-        test    eax, eax
-        _emit   75h
-        _emit   0fh
-        mov     dword ptr [g_walkCallback], 1
-        jmp     func_0046f370
-        ret
-    }
+void PushCallPauseSet1Jmp_00438f20(void) {
+    func_00440950(&g_data_004e4a1c);
+    if (g_framePauseFlag != 0) return;
+    g_walkCallback = (void (*)(void))1;
+    func_0046f370();
 }
