2329ae160299a35351c897d7ff5d5219951a792b
nathan-casabieille <nathan.casabieille1@gmail.com>
2026-05-24 23:34:12 +0200

    refactor: convert CallThreeSumLea_004c45d0 from naked asm to pure C

--- src/platform/win32/call_three_sum_lea.c
+++ src/platform/win32/call_three_sum_lea.c
@@ -27,23 +27,13 @@ extern unsigned int g_scaledInit_00542044;
 extern unsigned int g_state_00f9f7bc;
 extern unsigned int g_iat_004d21b0;
 
-__declspec(naked) void CallThreeSumLea_004c45d0(void) {
-    __asm {
-        push    esi
-        mov     esi, dword ptr [g_iat_004d21b0]
-        push    edi
-        push    0x2e
-        call    esi
-        push    8
-        mov     edi, eax
-        call    esi
-        push    4
-        add     edi, eax
-        call    esi
-        lea     eax, [eax + edi*2]
-        pop     edi
-        mov     dword ptr [g_state_00f9f7bc], eax
-        pop     esi
-        ret
-    }
+void CallThreeSumLea_004c45d0(void) {
+    typedef unsigned int (__stdcall *fn_t)(unsigned int);
+    fn_t fn = (fn_t)g_iat_004d21b0;
+    unsigned int s;
+    unsigned int c;
+    s = fn(0x2e);
+    s += fn(8);
+    c = fn(4);
+    g_state_00f9f7bc = c + s * 2;
 }
