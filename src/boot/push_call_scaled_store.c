075484709334f85340a0df7844805de4097b0517
nathan-casabieille <nathan.casabieille1@gmail.com>
2026-05-26 14:39:51 +0200

    refactor: lift 3 state_mask (+0x34) accesses

--- src/boot/push_call_scaled_store.c
+++ src/boot/push_call_scaled_store.c
@@ -21,5 +21,5 @@ extern int func_0049cb38(int, void *);
 extern void *g_dispatchSave1134_004146d0;
 void PushCallScaledStore_004143c0(void) {
     func_0049cb38((int)&g_dispatchSave1134_004146d0, 0x8f);
-    *(unsigned int *)(g_scaledInit_00542044 * 4 + 0x34) = g_baseSel_00542060;
+    ((ScenegraphNode *)(g_scaledInit_00542044 * 4))->state_mask = g_baseSel_00542060;
 }
